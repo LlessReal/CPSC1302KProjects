e35ac8f7b100d1de14b7740ba63be993c43a7d53
LlessReal <marshall3001.mm@gmail.com>
2025-01-18 14:53:48 -0500

    test

--- src/First.java
+++ src/First.java
@@ -1,9 +1,6 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class First {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
+        // Miguel Marshall
         System.out.println("Hello my friends!");
         System.out.println("Job: Web Developer");
         System.out.println("Why: Flexibility");
