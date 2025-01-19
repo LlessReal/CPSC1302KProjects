fe65374827468f2afb192942e4fbabff01ef1d0b
LlessReal <marshall3001.mm@gmail.com>
2025-01-19 00:56:13 -0500

    halting for now, don't got this really planned out

---
+++ Pets.java
@@ -0,0 +1,17 @@
+public class Pets {
+    private String petname;
+
+    public Pets(String petname) {
+        this.petname = petname;
+    }
+
+    public void toString() {
+        return petname;
+
+    }
+
+    public static void main(Strings[] args) {
+        System.out.println(toString());
+    }
+
+}
