8a3ace181f00f3e4883fa22e16ff1202ca22ad81
LlessReal <marshall3001.mm@gmail.com>
2025-01-19 23:31:38 -0500

    My Brain hurts

--- Pets.java
+++ Pets.java
@@ -1,17 +1,41 @@
+// Here are where you bring in
+import java.util.ArrayList;
+
+
 public class Pets {
-    private String petname;
+    // Fields
+    private String name1;
+    private String name2;
 
-    public Pets(String petname) {
-        this.petname = petname;
+    // Constructor
+    public Pets(String name) {
+        this.name1 = name;
     }
 
-    public void toString() {
-        return petname;
-
+    public void setPetName(String name) {
+        this.name2 = name;
     }
 
-    public static void main(Strings[] args) {
-        System.out.println(toString());
+    public String getPetName() {
+        return name2;
     }
 
+    // Method
+    public void showName() { // Code to show object's current status (just a name)
+        System.out.println("Pet Information:");
+        System.out.println("Name: " + name1);
+    }
+
+    // Main method (for demonstration)
+    public static void main(String[] args) {
+        // Creating an object of Pets
+        Pets Pet1 = new Pets("Joe Biden");
+        Pets Pet2 = new Pets("");
+        Pet1.showName(); // Calling a method on the object
+
+        Pet2.setPetName("Trump");
+        System.outprintln("");
+        System.out.println("Pet Information:");
+        System.out.println("Name: " + Pet2.getPetName());
+    }
 }
