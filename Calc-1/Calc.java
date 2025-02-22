c30c9e38d8780eaf7a389daaffe14a0440f889a9
LlessReal <marshall3001.mm@gmail.com>
2025-02-21 19:16:45 -0500

    Calc Project Done Gg ez (adding DecimalFormat next before I head to the next)

---
+++ Calc-1/Calc.java
@@ -0,0 +1,40 @@
+
+public class Calc {
+    private double firstnum;
+    private double secondnum;
+    // Constructor
+    public Calc() {
+        this.firstnum = 0;
+        this.secondnum = 0;
+    }
+
+    public void setNum1(double num) {
+        this.firstnum = num;
+    }
+    public void setNum2(double num) {
+        this.secondnum = num;
+    }
+
+    public double getNum1() {
+        return this.firstnum;
+    }
+    public double getNum2() {
+        return this.secondnum;
+    }
+    public double add() {
+        return this.firstnum + this.secondnum;
+    }
+    public double subtract() {
+        return this.firstnum - this.secondnum;
+    }
+    public double multiply() {
+        return this.firstnum * this.secondnum;
+    }
+    public double divide() {
+        return this.firstnum / this.secondnum;
+    }
+
+    public String toString() {
+        return "Displaying private data fields using toString():\nNum1: " + this.firstnum + "\nNum2: " + this.secondnum;
+    }
+}
---
+++ Pets-1/Pets1.java
@@ -0,0 +1,35 @@
+public class Pets1 {
+    // Fields
+    private String name; // Empty field for objects (for now)
+
+    // Constructor
+    public Pets(String name) {
+        this.name = name; // Current object's data
+    }
+
+    public void setPetName(String thenewname) {
+        this.name = thenewname;
+    }
+
+    public String getPetName() {
+        return this.name; // Returns current name
+    }
+    public String toString() {
+        String TheName = "Name: " + this.name; // Name: the name
+        return TheName; // This is called just puttin the object's name
+    } // toString() override
+
+    // Main method (for demonstration)
+    public static void main(String[] args) {
+        // Creating an object of Pets
+        Pets Pet1 = new Pets("Joe Bulldog"); // New object named Pet1 whose name is Joe Biden
+        Pets Pet2 = new Pets(""); // New object called Pet2 that has no name yet
+        Pet2.setPetName("TrumPawg"); // Use set method to set the name
+
+        System.out.println("Pet Information:");
+        System.out.println("Name: " + Pet1.getPetName()); // Could just put Pet1 but im utilizing get for the sake of the assignment
+        System.out.println(""); // New line
+        System.out.println("Pet Information:");
+        System.out.println(Pet2); // Using the toString() method
+    }
+}
