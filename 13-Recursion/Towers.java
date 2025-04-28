c00f429fd713ddfd79a550934e8c79ba08781e4e
LlessReal <marshall3001@gmail.com>
2025-04-28 17:35:33 -0400

    Initializing recursion assignment (like id remember this)

---
+++ 13-Recursion/Towers.java
@@ -0,0 +1,15 @@
+public class Towers {
+    private int numDiscs;
+
+    public Towers(int n) {
+        this.numDiscs = n;
+        this.moveDiscs(this.numDiscs, 1, 3, 2);
+    }
+    private void moveDiscs(int num, int fromPeg, int to Peg, int tempPeg) {
+        if (num > 0) {
+            moveDiscs(num - 1, fromPeg, tempPeg, toPeg);
+            System.out.println("Move a disc from peg " + fromPeg + " to peg " + toPeg);
+            moveDiscs(num - 1, tempPeg, toPeg, fromPeg);
+        }
+    }
+}
